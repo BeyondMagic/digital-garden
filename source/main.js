96a570ecada2d7898b046a61b1b6f4341484c6fb
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-03-26 22:42:02 -0300

    feat(main): add serve barebones

---
+++ source/main.js
@@ -0,0 +1,18 @@
+import { serve } from "bun";
+import { debug } from "./util";
+
+/**
+ * @param {Request} req
+ * @returns {Promise<Response>}
+ */
+async function fetch(req) {
+	return new Response("Not Found", { status: 404 });
+}
+
+const server = serve({
+	port: 3001,
+	hostname: "0.0.0.0",
+	fetch,
+});
+
+debug(`Listening on ${server.url}`);
