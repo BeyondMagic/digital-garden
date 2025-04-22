035dac39ac304b3b05b213040f10eea7334cc495
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-04-21 22:42:20 -0300

    feat(query): add language queries

--- source/database/query.js
+++ source/database/query.js
@@ -1,6 +1,34 @@
 import { sql } from "bun";
 
 /**
+ * Creates a language in the ISO 639-1 format in the database.
+ * @param {string} language - The language to be inserted.
+ * @returns {Promise<void>} A promise that resolves when the language is inserted.
+ */
+export async function insert_language(language) {
+	await sql`
+		INSERT INTO language (id)
+			VALUES (${language});
+	`;
+}
+
+/**
+ * Insert information about a language into the database.
+ * @param {Object} information - Information of the language to be documented.
+ * @param {string} information.id_for - ID of the language that is being documented.
+ * @param {string} information.id_from - ID of the language that the information is in.
+ * @param {string} information.name - Name of the langauge being documented.
+ * @param {string} information.description - Description of the language being documented.
+ * @returns {Promise<void>} Resolves when the language information is inserted.
+ */
+export async function insert_language_information({id_for, id_from, name, description}) {
+	await sql`
+		INSERT INTO language_information (id_for, id_from, name, description)
+			VALUES (${id_for}, ${id_from}, ${name}, ${description});
+	`;
+}
+
+/**
  * Delete the database.
  * @returns {Promise<void>} A promise that resolves when the database is deleted.
  */
