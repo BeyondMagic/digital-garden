124283f069bc69876d43f6261cecc8fbf8271a08
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-05-09 20:01:34 -0300

    function(util): add js tagged template

--- source/util.js
+++ source/util.js
@@ -2,6 +2,20 @@ import { $ } from "bun";
 
 
 /**
+ * Tagged template to parse Javascript string.
+ * @param {string} strings Outer content.
+ **/
+export function js (strings, ...values)
+{
+	return String.raw(
+		{
+			raw: strings
+		},
+		...values
+	).trim();
+}
+
+/**
  * Tagged template to parse HTML string.
  * @param {string} strings Outer content.
  **/
