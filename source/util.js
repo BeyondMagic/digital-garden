361c374ec58c8036eeb48237f4f84e855b3e0528
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-05-09 20:01:25 -0300

    function(util): add html tagged template

--- source/util.js
+++ source/util.js
@@ -1,5 +1,19 @@
 import { $ } from "bun";
 
+
+/**
+ * Tagged template to parse HTML string.
+ * @param {string} strings Outer content.
+ **/
+export function html (strings, ...values)
+{
+	return String.raw(
+		{
+			raw: strings
+		},
+		...values
+	).trim();
+}
 /**
  * Retrieve the root path of the GIT repository.
  **/
