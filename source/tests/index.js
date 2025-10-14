d5511c16e9cd550b0cda18d6bfc3e5cf025e62be
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-10-14 18:45:45 -0300

    fix(tests): check 'type' instead of 'status'

--- source/tests/index.js
+++ source/tests/index.js
@@ -1,15 +1,16 @@
 import { sql } from "bun";
 
 /**
+ * Check if a database object exists.
  * @param {string} name Name of the table to check.
- * @param {'table'|'view'|'status'} [type='table'] Type of the object to check.
+ * @param {'table'|'view'|'type'} [type='table'] Type of the object to check.
  * @returns {Promise<boolean>} True if the table exists, false otherwise.
  */
 async function exists(name, type = 'table') {
 	const queries = {
 		table: () => sql`SELECT * FROM pg_tables WHERE tablename = ${name}`,
 		view: () => sql`SELECT * FROM pg_views WHERE viewname = ${name}`,
-		status: () => sql`SELECT * FROM pg_stat_all_tables WHERE relname = ${name}`
+		type: () => sql`SELECT * FROM pg_type WHERE typname = ${name}`
 	};
 	const result = await queries[type]();
 	return result.length > 0;
