63f82634bc51c69f31c9f4049761d54ed13eb216
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-10-18 21:24:09 -0300

    feat(query): add schema creation

--- source/database/query/create/index.js
+++ source/database/query/create/index.js
@@ -1,7 +1,24 @@
-import tables from '@/database/query/create/tables';
-import types from '@/database/query/create/types';
+import { tables } from '@/database/query/create/tables';
+import { types } from '@/database/query/create/types';
+import { exists } from '@/database/query/util';
+import { assert } from '@/logger';
 
-export default {
-	tables,
-	types,
+async function schema() {
+	await types.domain();
+	await types.status();
+
+	await tables.domain();
+	await tables.asset();
+}
+
+schema.test = async function () {
+	types.domain.test();
+	types.status.test();
+
+	tables.domain.test();
+	tables.asset.test();
+};
+
+export const create = {
+	schema
 };
--- source/database/query/remove/index.js
+++ source/database/query/remove/index.js
@@ -1,5 +1,24 @@
-import types from '@/database/query/remove/types';
+import { types } from '@/database/query/remove/types';
+import { tables } from '@/database/query/remove/tables';
+import { exists } from '@/database/query/util';
+import { assert } from '@/logger';
 
-export default {
-	types,
+async function schema() {
+	await tables.asset();
+	await tables.domain();
+
+	await types.status();
+	await types.domain();
+}
+
+schema.test = async function () {
+	types.status.test();
+	types.domain.test();
+
+	tables.domain.test();
+	tables.asset.test();
+}
+
+export const remove = {
+	schema,
 };
