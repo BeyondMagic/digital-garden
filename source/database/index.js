d0d607a1a13c424f0d70a7090d4a483a81060bcd
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-08 01:13:16 -0300

    fix(license): update copyright year in license header

--- source/database/index.js
+++ source/database/index.js
@@ -1,6 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 João V. Farias (beyondmagic) <beyondmagic@mail.ru>
- *
+ * SPDX-FileCopyrightText: 2026 João V. Farias (beyondmagic) <beyondmagic@mail.ru>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
