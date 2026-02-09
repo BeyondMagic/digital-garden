a802c4a610d1157ca6ab7a08aecda2bfde53bffe
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-09 18:14:19 -0300

    fix(database): correct existence checks for database tables and types

--- source/database/query/create/index.js
+++ source/database/query/create/index.js
@@ -1,6 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 João V. Farias (beyondmagic) <beyondmagic@mail.ru>
- *
+ * SPDX-FileCopyrightText: 2025-2026 João V. Farias (beyondmagic) <beyondmagic@mail.ru>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
@@ -8,50 +7,47 @@ import { tables } from "@/database/query/create/tables";
 import { types } from "@/database/query/create/types";
 
 async function schema() {
-
-	if (await types.domain.exists())
+	if (!await types.domain.exists())
 		await types.domain();
-
-	if (await types.subject_status.exists())
+	if (!await types.subject_status.exists())
 		await types.subject_status();
-
-	if (await tables.domain.exists())
+	if (!await tables.domain.exists())
 		await tables.domain();
-	if (await tables.asset.exists())
+	if (!await tables.asset.exists())
 		await tables.asset();
-	if (await tables.language.exists())
+	if (!await tables.language.exists())
 		await tables.language();
-	if (await tables.language_information.exists())
+	if (!await tables.language_information.exists())
 		await tables.language_information();
-	if (await tables.asset_information.exists())
+	if (!await tables.asset_information.exists())
 		await tables.asset_information();
-	if (await tables.tag.exists())
+	if (!await tables.tag.exists())
 		await tables.tag();
-	if (await tables.tag_requirement.exists())
+	if (!await tables.tag_requirement.exists())
 		await tables.tag_requirement();
-	if (await tables.tag_information.exists())
+	if (!await tables.tag_information.exists())
 		await tables.tag_information();
-	if (await tables.domain_tag.exists())
+	if (!await tables.domain_tag.exists())
 		await tables.domain_tag();
-	if (await tables.content.exists())
+	if (!await tables.content.exists())
 		await tables.content();
-	if (await tables.content_link.exists())
+	if (!await tables.content_link.exists())
 		await tables.content_link();
-	if (await tables.garden.exists())
+	if (!await tables.garden.exists())
 		await tables.garden();
-	if (await tables.garden_information.exists())
+	if (!await tables.garden_information.exists())
 		await tables.garden_information();
-	if (await tables.author.exists())
+	if (!await tables.author.exists())
 		await tables.author();
-	if (await tables.author_connection.exists())
+	if (!await tables.author_connection.exists())
 		await tables.author_connection();
-	if (await tables.author_garden.exists())
+	if (!await tables.author_garden.exists())
 		await tables.author_garden();
-	if (await tables.author_domain.exists())
+	if (!await tables.author_domain.exists())
 		await tables.author_domain();
-	if (await tables.author_content.exists())
+	if (!await tables.author_content.exists())
 		await tables.author_content();
-	if (await tables.module.exists())
+	if (!await tables.module.exists())
 		await tables.module();
 }
 
