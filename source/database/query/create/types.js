fa9e1c4870e9915b504266b5a725414a106a2973
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-09 19:42:54 -0300

    fix(types): update copyright year and add exists

--- source/database/query/create/types.js
+++ source/database/query/create/types.js
@@ -1,10 +1,10 @@
 /*
- * SPDX-FileCopyrightText: 2025 João V. Farias (beyondmagic) <beyondmagic@mail.ru>
- *
+ * SPDX-FileCopyrightText: 2025-2026 João V. Farias (beyondmagic) <beyondmagic@mail.ru>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 import { sql } from "bun";
+import { util } from "@/database/query/util";
 
 async function domain() {
 	await sql`
@@ -12,10 +12,18 @@ async function domain() {
 	`;
 }
 
+domain.exists = async function () {
+	return util.exists('type_domain', 'type');
+}
+
 async function subject_status() {
 	await sql`
 		CREATE TYPE TYPE_SUBJECT_STATUS AS ENUM ('PUBLIC', 'PRIVATE', 'ARCHIVED', 'DELETED');
 	`;
+}
+
+subject_status.exists = async function () {
+	return util.exists('type_subject_status', 'type');
 }
 
 export const types = {
