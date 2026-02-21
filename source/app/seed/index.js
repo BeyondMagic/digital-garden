07eba1b9196e4dc6db4c60067f83ebdcd264ef18
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-21 02:26:43 -0300

    refactor(seed): streamline seeding process and remove unused domain functions

--- source/app/seed/index.js
+++ source/app/seed/index.js
@@ -3,143 +3,53 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-/**
- * @import { RowIdentifier, DomainInput } from "@/database/query"
- * @import { Capability } from "@/module/api"
- */
-
 import { insert } from "@/database/query/insert"
-import { remove } from "@/database/query/remove"
-import { json_to_response } from "@/module/api";
-import { capability } from "@/module/api/capability";
+import { select } from "@/database/query/select"
 import { create_debug } from "@/logger";
 
 const debug = create_debug(import.meta.path);
 
-/**
- * @param {Request} request
- * @returns {Promise<Response>}
- */
-async function add_domain(request) {
+export async function tables() {
 
-	const body = /** @type {DomainInput} */ (await request.json());
+	debug("Checkinig if seeding for tables was set.", { step: { current: 1, max: 2 } });
 
-	const {
-		id_domain_parent,
-		id_domain_redirect,
-		kind,
-		slug,
-		status,
-	} = body;
-
-	const id = await insert.domain({
-		id_domain_parent,
-		id_domain_redirect,
-		kind,
-		slug,
-		status,
-	});
-
-	const result = { id_domain: id };
-
-	return json_to_response(result);
-}
-
-/**
- * @param {Request} request
- * @returns {Promise<Response>}
- */
-async function remove_domain(request) {
-
-	const body = /** @type {{id_domain: number}} */ (await request.json());
-
-	await remove.domain({
-		id: body.id_domain,
-	});
-
-	return new Response(null, { status: 204 });
-}
-
-/**
- * @param {Request} request
- * @returns {Promise<Response>}
- */
-async function update_domain(request) {
-	// TO-DO
-	return new Response("Not implemented", { status: 501, headers: { "content-type": "text/plain" } });
-}
-
-/**
- * @type {Array<Capability<any>>}
- */
-const capabilities = [
-	{
-		method: "POST",
-		slug: "domain/add",
-		scope: "garden",
-		name: "Add Domain",
-		description: "Adds a new domain to the garden.",
-		handler: add_domain,
-		deprecation: null,
-		input: {
-			id_domain_parent: "number | null",
-			id_domain_redirect: "number | null",
-			kind: "string",
-			slug: "string",
-			status: "string",
-			token: "string"
-		},
-		output: {
-			id_domain: "number"
-		}
-	},
-	{
-		method: "DELETE",
-		slug: "domain/remove",
-		scope: "garden",
-		name: "Remove Domain",
-		description: "Removes a domain from the garden.",
-		handler: remove_domain,
-		deprecation: null,
-		input: {
-			id_domain: "number",
-			token: "string"
-		},
-		output: null
-	},
-	{
-		method: "PUT",
-		slug: "domain/update",
-		scope: "garden",
-		name: "Update Domain",
-		description: "Updates domain info in the garden.",
-		handler: update_domain,
-		deprecation: null,
-		input: {
-			id_domain: "number",
-			id_domain_parent: "number | null",
-			id_domain_redirect: "number | null",
-			kind: "string",
-			slug: "string",
-			status: "string",
-			token: "string"
-		},
-		output: null
+	if (await select.count("garden") > 0) {
+		debug("Database already seeded.", { step: { current: 2, max: 2 } });
+		return;
 	}
-]
 
-export async function setup() {
-	debug("Setting up server capabilities...", { step: { current: 1, max: 3 } });
+	debug("Database not seeded.", { step: { current: 2, max: 2 } });
 
-	for (const cap of capabilities)
-		capability.register(cap.method, cap.slug, cap);
+	debug("Seeding tables..", { step: { current: 1, max: 2 } });
 
-	debug("Server capabilities setup complete.", { step: { current: 2, max: 3 } });
+	const domain_root = await insert.domain({
+		id_domain_parent: null,
+		id_domain_redirect: null,
+		kind: "SUBDOMAIN",
+		slug: null,
+		status: "PRIVATE",
+	});
 
-	// set up initial domains
-}
+	const asset_admin_profile_picture = await insert.asset({
+		id_domain: domain_root,
+		slug: "admin-profile-picture.png",
+		data: {
+			blob: Bun.file("../public/digital-garden-girl.png")
+		}
+	});
 
-export const seed = {
-	capabilities,
-	setup
+	const author_admin = await insert.author({
+		id_asset: asset_admin_profile_picture,
+		email: "admin@localhost",
+		name: "Admin",
+		password: "admin",
+	});
+
+	insert.garden({
+		id_domain: domain_root,
+		id_asset: asset_admin_profile_picture,
+		id_author: author_admin,
+	})
+
+	debug("Done.", { step: { current: 2, max: 2 } });
 }
