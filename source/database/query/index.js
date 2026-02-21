38ccb5882037a037394bd07fd0d62fb6ce33653b
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-21 17:00:23 -0300

    refactor(domain): replace inline type definition with DomainKind typedef for clarity

--- source/database/query/index.js
+++ source/database/query/index.js
@@ -48,10 +48,14 @@
  */
 
 /**
+ * @typedef {"SUBDOMAIN" | "ROUTER"} DomainKind - Possible kinds of domains.
+ */
+
+/**
  * @typedef {Object} DomainInput
  * @property {number | null} id_domain_parent - ID of the parent domain (nullable).
  * @property {number | null} id_domain_redirect - ID of the domain to redirect to (nullable).
- * @property {"SUBDOMAIN" | "ROUTER"} kind - Kind of the domain.
+ * @property {DomainKind} kind - Kind of the domain.
  * @property {string | null} slug - Unique slug for the domain.
  * @property {SubjectStatus} status - Status of the domain.
  */
