f3c9d6b157cb211c507ff97232af80a86f74906b
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-21 03:12:16 -0300

    refactor(seed): improve logging and error handling in table seeding process

--- source/app/seed/index.js
+++ source/app/seed/index.js
@@ -3,17 +3,21 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { insert } from "@/database/query/insert"
-import { select } from "@/database/query/select"
-import { create_debug } from "@/logger";
+import { sql } from "bun";
+import { insert } from "@/database/query/insert";
+import { select } from "@/database/query/select";
+import { create_critical, create_debug, create_info } from "@/logger";
 
 const debug = create_debug(import.meta.path);
+const critical = create_critical(import.meta.path);
+const info = create_info(import.meta.path);
 
 export async function tables() {
+	debug("Checkinig if seeding for tables was set.", {
+		step: { current: 1, max: 2 },
+	});
 
-	debug("Checkinig if seeding for tables was set.", { step: { current: 1, max: 2 } });
-
-	if (await select.count("garden") > 0) {
+	if ((await select.count("garden")) > 0) {
 		debug("Database already seeded.", { step: { current: 2, max: 2 } });
 		return;
 	}
@@ -22,34 +26,50 @@ export async function tables() {
 
 	debug("Seeding tables..", { step: { current: 1, max: 2 } });
 
-	const domain_root = await insert.domain({
-		id_domain_parent: null,
-		id_domain_redirect: null,
-		kind: "SUBDOMAIN",
-		slug: null,
-		status: "PRIVATE",
-	});
+	try {
+		const domain_root = await insert.domain({
+			id_domain_parent: null,
+			id_domain_redirect: null,
+			kind: "SUBDOMAIN",
+			slug: null,
+			status: "PRIVATE",
+		});
 
-	const asset_admin_profile_picture = await insert.asset({
-		id_domain: domain_root,
-		slug: "admin-profile-picture.png",
-		data: {
-			blob: Bun.file("../public/digital-garden-girl.png")
-		}
-	});
+		const asset_admin_profile_picture_path = `${import.meta.dir}/../public/image/digital-garden-girl.png`
+		info(`Asset path\t→ ${asset_admin_profile_picture_path}`);
 
-	const author_admin = await insert.author({
-		id_asset: asset_admin_profile_picture,
-		email: "admin@localhost",
-		name: "Admin",
-		password: "admin",
-	});
+		const file = Bun.file(asset_admin_profile_picture_path);
+		info(`Asset file\t→ ${file}`);
 
-	insert.garden({
-		id_domain: domain_root,
-		id_asset: asset_admin_profile_picture,
-		id_author: author_admin,
-	})
+		const asset_admin_profile_picture = await insert.asset({
+			id_domain: domain_root,
+			slug: "admin-profile-picture.png",
+			data: {
+				blob: Bun.file(asset_admin_profile_picture_path),
+			},
+		});
+
+		const author_admin = await insert.author({
+			id_asset: asset_admin_profile_picture,
+			email: "admin@localhost",
+			name: "Admin",
+			password: "admin",
+		});
+
+		insert.garden({
+			id_domain: domain_root,
+			id_asset: asset_admin_profile_picture,
+			id_author: author_admin,
+		});
+	} catch (err) {
+		critical("Error seeding tables.", { step: { current: 2, max: 2 } });
+		critical(err);
+		return;
+	}
 
 	debug("Done.", { step: { current: 2, max: 2 } });
 }
+
+export const seed = {
+	tables,
+};
