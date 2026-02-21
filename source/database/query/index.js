96db70c3921bd7a7285247a20864161aa529d1fb
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-21 01:58:48 -0300

    fix(database): remove unused reset import from query module

--- source/database/query/index.js
+++ source/database/query/index.js
@@ -249,13 +249,11 @@
 import { create } from "@/database/query/create";
 import { insert } from "@/database/query/insert";
 import { remove } from "@/database/query/remove";
-import { reset } from "@/database/query/reset";
 import { select } from "@/database/query/select";
 
 export const query = {
 	create,
 	insert,
-	reset,
 	select,
 	remove,
 };
